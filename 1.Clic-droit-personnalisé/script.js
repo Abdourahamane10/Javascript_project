823635b65b89909ae9c9a000bfed37de54f026bb
Abdourahamane <amdroudjae@gmail.com>
2023-09-21 09:28:47 +0200

    Changement de la couleur de fond selon le bouton du menu personnalisé cliqué

--- 1.Clic-droit-personnalisé/script.js
+++ 1.Clic-droit-personnalisé/script.js
@@ -8,3 +8,18 @@ function handleCustomMenu(e){
     customMenu.style.display = "block"; //Affichage du menu
     customMenu.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`; //Faire apparaître le menu là où il y'a la souris
 }
+
+document.addEventListener("click", handleDocumentClick);
+
+function handleDocumentClick(e){
+    customMenu.style.display = "none";
+}
+
+customMenuBtns.forEach(btn => {
+    btn.addEventListener("click", handleCustomMenuFeature);
+});
+
+function handleCustomMenuFeature(e){
+    e.stopPropagation(); //On stope la propagation de l'évènement "click" pour pas que tous les écouteurs de cet évènement soient exécutés afin de pouvoir garder le menu lorsqu'on clique sur un bouton
+    document.body.style.backgroundColor = `${e.target.dataset.color}`; //On récupère la couleur du boutton cliqué
+}
