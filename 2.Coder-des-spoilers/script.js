f3d6adbde27401bd3183986dfd9b825f97778ece
Abdourahamane <amdroudjae@gmail.com>
2023-09-22 09:24:27 +0200

    Ajout de la classe "js-spoiler-revealed" lorsqu'on clique sur les éléments contenant la classe "js-spoiler"

---
+++ 2.Coder-des-spoilers/script.js
@@ -0,0 +1,6 @@
+const jsSpoilers = document.querySelectorAll(".js-spoiler");
+jsSpoilers.forEach(spoiler => spoiler.addEventListener("click", toggleSpoiler));
+
+function toggleSpoiler(e){
+    e.target.classList.add("js-spoiler-revealed");
+}
